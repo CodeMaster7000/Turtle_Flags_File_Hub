07546f62010c4797a536b3a00f2cc8eb35824078
Code-Master1234 <95772109+Code-Master1234@users.noreply.github.com>
2021-12-19 13:30:28 +0000

    Add files via upload

---
+++ Indian Flag.py
@@ -0,0 +1,87 @@
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+t.shape("arrow")
+c = 24
+
+s.bgcolor("white")
+t.speed(4)
+s.setup(800,500)
+
+t.up()
+t.goto(-400, 250)
+t.down()
+
+t.color("dark orange")
+t.begin_fill()
+t.forward(800)
+t.right(90)
+t.forward(167)
+t.right(90)
+t.forward(800)
+t.end_fill()
+t.up()
+
+t.left(90)
+t.forward(167)
+
+t.color("green")
+t.begin_fill()
+t.forward(167)
+t.left(90)
+t.forward(800)
+t.left(90)
+t.forward(167)
+t.end_fill()
+
+t.speed(5)
+t.up()
+t.goto(70,0)
+t.down()
+t.color("navy")
+t.begin_fill()
+t.circle(70)
+t.end_fill()
+
+t.up()
+t.goto(60,0)
+t.down()
+t.color("white")
+t.begin_fill()
+t.circle(60)
+t.end_fill()
+
+t.speed(0)
+t.up()
+t.goto(-57,-8)
+t.down()
+t.color("navy")
+for i in range(c):
+    t.speed(0)
+    t.begin_fill()
+    t.circle(3)
+    t.end_fill()
+    t.up()
+    t.forward(15)
+    t.right(15)
+    t.down()
+   
+t.up()
+t.goto(20,0)
+t.down()
+t.begin_fill()
+t.circle(20)
+t.end_fill()
+
+t.speed(4)
+t.up()
+t.goto(0,0)
+t.down()
+t.pensize(2)
+for i in range(c):
+    t.forward(60)
+    t.backward(60)
+    t.left(15)
+
+t.hideturtle()
